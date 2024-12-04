d396d5dc8553d7c83ad3ff0925376d14fa54a13a
ZhangYuZai <s969980719@vip.qq.com>
2024-12-05 01:07:21 +0800

    示例

--- src/components/pdfTemplate.vue
+++ src/components/pdfTemplate.vue
@@ -6,6 +6,7 @@
 
 <script>
 import * as pdfjsLib from "pdfjs-dist";
+import { PDFDocument, rgb } from 'pdf-lib';
 // 设置 PDF.js 的 worker 文件路径
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdfjs/pdf.worker.mjs';
 export default {
@@ -27,7 +28,7 @@ export default {
       const loadingTask = pdfjsLib.getDocument(this.pdfUrl);
       const pdf = await loadingTask.promise;
       const page = await pdf.getPage(2);
-      const viewport = page.getViewport({ scale: 1.5 });
+      const viewport = page.getViewport({ scale: 1 });
       const canvas = this.$refs.pdfCanvas;
       const context = canvas.getContext("2d");
       canvas.height = viewport.height;
@@ -38,13 +39,49 @@ export default {
       };
       await page.render(renderContext).promise;
       const textContent = await page.getTextContent();
+      
       console.log("textContent ==> ", textContent)
       const pageText = textContent.items.map((item) => item.str).join(" ");
       let fullText = `Page ${1}:\n${pageText}\n\n`;
 
       console.log(`Extracted text from page ${1}`);
       console.log("fullText ==> ", fullText)
+      context.fillStyle = '#FFFFFF'
+      context.fillRect(36, viewport.height - 695.2201 - 11.5,229.128,12)
+      context.fillStyle = '#000000'
+      context.font = "bold 12px sans-serif"
+      context.fillText("123456", 36, viewport.height - 695.2201 - 11.5)
+      console.log("drawRect")
+      this.modifyOriginalPdf(textContent, this.pdfUrl, page)
     },
+
+    async modifyOriginalPdf(translatedData, originalPdfUrl, page) {
+      const existingPdfBytes = await fetch(originalPdfUrl).then((res) => res.arrayBuffer());
+      const pdfDoc = await PDFDocument.load(existingPdfBytes)
+      const viewport = page.getViewport({ scale: 1 });
+      for (const item of translatedData.items) {
+        console.log("item ==> ", item, viewport.height)
+        const pdfPage = pdfDoc.getPage(1);
+
+      //   // 覆盖原文本内容
+        // 绘制翻译后的文本
+        pdfPage.drawText("item.text???", {
+          x: 36,
+          y: 695.2201,
+          size: 12,
+          color: rgb(0, 0, 0), // 黑色文字
+        });
+      }
+
+        const pdfBytes = await pdfDoc.save();
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'modified_original.pdf';
+        link.click();
+    }
   }
 };
 </script>
